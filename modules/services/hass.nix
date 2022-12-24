5a3ce97593db1d60f28eba1038fb53fccdfc45f5
Nathan Viets <nathan.g.viets@gmail.com>
2022-12-24 18:25:46 +0000

    R libs

--- modules/services/hass.nix
+++ modules/services/hass.nix
@@ -8,6 +8,10 @@
   };
   services.home-assistant = with pkgs; {
     enable = true;
+    openFirewall = true;
+    extraPackages = python3Packages: with python3Packages; [
+      pyatv
+    ];
     extraComponents = [
       "awair"
       "speedtestdotnet"
@@ -18,13 +22,18 @@
         api_version = 2;
         ssl = false;
         verify_ssl = false;
-        host = "mini";
+        host = "localhost";
         port = 8086;
-        token =
-          "YBNgGXuBCg5caAbSEpk8zOcs_TQzZ0720k7cB2_aTN0mLLEYm0LHhDHhkCHUoUJT-dzVhlFk8NgWTdbBX7LeSQ==";
-        organization = "77baf2d7fdfe57b3";
+        token = "aDFEu43Kbh6z5FjgPrANcoR5EdHAmBz35iOXZgAzqHPQFsit5e8agR7kuZZg2L9G9mJFmiWmZFgkJd0so6_F1A==";
+        organization = "dab3742f664a79c9";
         bucket = "hass";
+        tags = {
+          source = "hass";
+        };
         default_measurement = "state";
+        include = {
+          domains = "sensor";
+        };
       };
     };
   };
